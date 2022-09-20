cbca91d284dc5cfda062e39a5640f0980ea52b5b
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2022-09-20 20:54:52 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#f7444e",
+          secondary: "#0c0c0c",
+          accent: "#002c3e",
+          neutral: "#D4CED0",
+          "base-100": "#ffffff",
+        },
+      },
+      // "dark",
+      // "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
