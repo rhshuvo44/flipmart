eacec0ab9722daa42059b5a7aea7b142d6ecd44c
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2022-09-20 21:26:01 +0600

    banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,20 +1,23 @@
 import React from "react";
 import bg from "../../../img/slide-img1.jpg";
+import './Banner.css'
 
 const Banner = () => {
   return (
-    <div
-      className="hero min-h-screen"
+    <div 
+      className="hero min-h-screen banner"
       style={{ backgroundImage: `url(${bg})` }}
     >
-      <div className="hero-overlay bg-opacity-60"></div>
+      <div className=" bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-md">
-          <h1 className="mb-5 text-4xl font-bold">Flipmart Hot Collection</h1>
+        <div className="text-white">
+          <h1 className="mb-5 text-5xl font-bold">Flipmart Hot Collection</h1>
+          <div className="max-w-md mx-auto">
           <p className="mb-5">
             Pellentesque habitant morbi tristique senectus et netus et malesuada
           </p>
           <button className="btn btn-primary">Shop now</button>
+          </div>
         </div>
       </div>
     </div>
